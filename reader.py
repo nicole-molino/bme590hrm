eaf0458c1fed80cdcb77087575894e549c15f67d
Nicole Molino <nkm12@duke.edu>
2018-10-26 00:29:38 -0400

    Add logging to reader

--- reader.py
+++ reader.py
@@ -7,13 +7,24 @@ def readinfile(file):
     Returns:
         data (tuple): numpy array containing voltage and time data
 
+    Raises:
+        ValueError: if non-numbers in file and breaks code
+
     """
+    import logging
+
+    logging.basicConfig(filename="HRMLogging.txt",
+                        format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.DEBUG)
 
     import numpy as np
     try:
         csv_data = np.loadtxt(file, delimiter=',')
 
     except ValueError:
+        logging.warning("Tried to input non-numbers, process stopped")
         raise ValueError("Can only input numbers")
+
 
     return csv_data
