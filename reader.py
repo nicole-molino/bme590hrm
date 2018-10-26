c665032315d7f35681f2f82c7c018520ef903a8d
Nicole Molino <nkm12@duke.edu>
2018-10-26 00:07:42 -0400

    fixed pep8

--- reader.py
+++ reader.py
@@ -11,10 +11,9 @@ def readinfile(file):
 
     import numpy as np
     try:
-        csv_data = np.loadtxt(file, delimiter= ',')
+        csv_data = np.loadtxt(file, delimiter=',')
 
     except ValueError:
         raise ValueError("Can only input numbers")
 
     return csv_data
-
--- test_reader.py
+++ test_reader.py
@@ -2,9 +2,12 @@ from reader import readinfile
 import pytest
 import numpy
 
+
 def test_reader():
     data = readinfile('readertestdata.csv')
-    numpy.testing.assert_array_equal(data, ([1.0, 12], [9, 18], [4.4, 3], [6, 4], [1.44, 6]))
+    numpy.testing.assert_array_equal(data, ([1.0, 12], [9, 18],
+                                            [4.4, 3], [6, 4], [1.44, 6]))
+
 
 def test_reader_ValueError():
     with pytest.raises(ValueError):
