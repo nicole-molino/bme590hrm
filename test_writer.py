b33d3d9b58cebea20ecf2beca241a3586040bcdc
Nicole Molino <nkm12@duke.edu>
2018-10-26 01:21:49 -0400

    Add initial writer and testwriter files

---
+++ test_writer.py
@@ -0,0 +1,20 @@
+from writer import writefile
+
+import json
+
+def test_writer():
+    dict = {}
+    dict["h"] = 6
+    dict["cat"] = 'dog'
+    dict["favorite number"] = 22
+
+    file = writefile(dict, 'testingjson')
+
+    with open('testingjson.json', 'r') as prac:
+        readdict = json.load(prac)
+
+    assert readdict == dict
+
+
+
+
---
+++ writer.py
@@ -0,0 +1,22 @@
+import json
+import logging
+
+logging.basicConfig(filename="HRMLogging.txt",
+                    format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.DEBUG)
+
+
+def writefile(metrics, file):
+    """write dict into file using json
+
+    Args:
+         metrics (dict): dictionary from processor with data
+         file (json): name of file to write to
+
+    Returns:
+        file (JSON file): returns file with desired name
+    """
+    with open((file + '.json') , 'w') as filename:
+        json.dump(metrics, filename)
+
